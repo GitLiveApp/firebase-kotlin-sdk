8a69d8c2dda15a8dc6b980650e12b3b6799acc79
magnumrocha <magnum.rocha@prozis.com>
2020-03-30 19:16:59 +0100

    update coroutines to version 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,19 +38,19 @@ subprojects {
     afterEvaluate  {
         dependencies {
             "commonMainImplementation"(kotlin("stdlib-common"))
-            "commonMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.4")
+            "commonMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
             "jsMainImplementation"(kotlin("stdlib-js"))
 
-            "jsMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.4")
+            "jsMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.5")
             "androidMainImplementation"(kotlin("stdlib-jdk8"))
-            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.4")
+            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.5")
             //"jvmMainImplementation"(kotlin("stdlib-jdk8"))
-            //"jvmMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+            //"jvmMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
             //"jvmMainApi"("app.teamhub:firebase-java:0.3.1")
-            //"jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.4")
-            "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.4")
-            "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.3.4")
+            //"jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.5")
+            "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
+            "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.3.5")
         }
     }
 }
