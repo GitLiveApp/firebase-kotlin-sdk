a50caae0cad609afcf6f3775a0ec9e6618af85e0
sadevelopment <marcus@squareapple.co.uk>
2020-04-16 12:08:42 +0100

    Update to firebase-java-sdk 0.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -146,7 +146,7 @@ subprojects {
             "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.4")
             "jvmMainImplementation"(kotlin("stdlib-jdk8"))
             "jvmMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-            "jvmMainApi"("dev.gitlive:firebase-java-sdk:0.3.1")
+            "jvmMainApi"("dev.gitlive:firebase-java-sdk:0.3.2")
             "jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.4")
             "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.4")
             "iosMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.3.4")
