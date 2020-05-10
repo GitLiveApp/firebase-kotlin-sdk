1b7b8c1da80c18b325793c3c521307d8a1d37f10
Nick Williams <nbransby@gmail.com>
2020-05-10 20:59:12 +0100

    bump minor version numbers

--- firebase-auth/src/androidMain/kotlin/dev/gitlive/firebase/auth/auth.kt
+++ firebase-auth/src/androidMain/kotlin/dev/gitlive/firebase/auth/auth.kt
@@ -23,7 +23,6 @@ actual class FirebaseAuth internal constructor(val android: com.google.firebase.
 
     actual suspend fun signInWithEmailAndPassword(email: String, password: String) =
         AuthResult(android.signInWithEmailAndPassword(email, password).await())
-        //AuthResult(android.signInWithEmailAndPassword(email, password).await())
 
     actual suspend fun signInWithCustomToken(token: String) =
         AuthResult(android.signInWithCustomToken(token).await())
--- firebase-common/build.gradle.kts
+++ firebase-common/build.gradle.kts
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 GitLive Ltd.  Use of this source code is governed by the Apache 2.0 license.
  */
 
-version = "0.1.0"
+version = "0.2.0"
 
 plugins {
     id("com.android.library")
