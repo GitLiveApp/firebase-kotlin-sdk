0f63e61a358b6f1bea02e9bfd5a66ed544d14bf8
sadevelopment <marcus@squareapple.co.uk>
2020-05-20 23:21:41 +0100

    Add emulator plugin - want to see what happens on git before refactoring it

--- firebase-common/build.gradle.kts
+++ firebase-common/build.gradle.kts
@@ -9,6 +9,18 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     kotlin("plugin.serialization") version "1.3.71"
+    id("com.quittle.android-emulator") version "0.2.0"
+}
+
+buildscript {
+    repositories {
+        jcenter()
+        google()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.1")
+    }
 }
 
 android {
@@ -32,6 +44,19 @@ android {
         pickFirst("META-INF/AL2.0")
         pickFirst("META-INF/LGPL2.1")
     }
+}
+
+// Optional configuration
+androidEmulator {
+    emulator {
+        name("givlive_emulator")
+        sdkVersion(28)
+        abi("x86_64")
+        includeGoogleApis(true) // Defaults to false
+    }
+
+    headless(true) // Defaults to false but should be set to true for most CI systems
+    logEmulatorOutput(true) // Defaults to false but can be enabled to have emulator output logged for debugging.
 }
 
 kotlin {
