b15544d9b90f486195f6c9784ebf117050675d1f
Andrew Reed <andrew_reed@hotmail.com>
2020-07-01 17:55:57 +0100

    Added token to script

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,11 @@ val minSdkVersion by extra(16)
 
 tasks {
     val downloadIOSFirebaseZipFile by creating(Download::class) {
-        header("Authorization", "token ${System.getenv("token")}")
         src("https://github.com/firebase/firebase-ios-sdk/releases/download/6.17.0/Firebase-6.17.0.zip")
         dest(File(buildDir, "Firebase-6.17.0.zip"))
+        if (System.getenv("token") != null) {
+            username(System.getenv("token"))
+        }
         overwrite(false)
     }
 
