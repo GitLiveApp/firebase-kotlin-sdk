937b8edfa6225241ea31b334194c3e31b409cc4a
nbransby <nbransby@gmail.com>
2020-08-03 18:02:15 +0100

    fix require statements in js

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,7 @@ subprojects {
                 into.createNewFile()
                 into.writeText(
                     from.readText()
-                        .replace("require('firebase-", "require('@gitlive/firebase-")
+                        .replace("require('firebase-kotlin-sdk-", "require('@gitlive/firebase-")
                     //                .replace("require('kotlinx-serialization-kotlinx-serialization-runtime')", "require('@gitlive/kotlinx-serialization-runtime')")
                 )
             }
