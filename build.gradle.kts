bef7b418f583fe797ec12be7144f98f783a2bb55
Andrew Reed <andrew_reed@hotmail.com>
2020-08-05 15:20:04 +0100

    Increased java sdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -204,7 +204,7 @@ subprojects {
         dependencies {
             "jvmMainImplementation"(kotlin("stdlib-jdk8"))
             "jvmMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-            "jvmMainApi"("dev.gitlive:firebase-java-sdk:0.4.3")
+            "jvmMainApi"("dev.gitlive:firebase-java-sdk:0.4.4")
             "jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.3.7") {
                 exclude("com.google.android.gms")
             }
