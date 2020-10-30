d8fb922d76f47a38b534eab9c0d7ca23a5bb3860
Gijs van Veen <daeda88@gmail.com>
2020-10-30 16:04:00 +0100

    Upgrade Serialization

--- firebase-common/build.gradle.kts
+++ firebase-common/build.gradle.kts
@@ -63,7 +63,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
             }
         }
         val androidMain by getting {
