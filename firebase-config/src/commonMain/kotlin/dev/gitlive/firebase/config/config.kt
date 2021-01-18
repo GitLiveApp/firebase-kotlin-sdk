dac0b9360e3822cdc8c373badb1f2e76d6dec79e
corrado4eyes <corradoquattrocchi@hotmail.it>
2021-01-18 07:25:53 +0100

    Add method `fromString` to enums. Made expiration parameter Long. Add Exceptions.

--- firebase-config/src/commonMain/kotlin/dev/gitlive/firebase/config/config.kt
+++ firebase-config/src/commonMain/kotlin/dev/gitlive/firebase/config/config.kt
@@ -2,6 +2,7 @@ package dev.gitlive.firebase.config
 
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseApp
+import dev.gitlive.firebase.FirebaseException
 
 /** Returns the [FirebaseRemoteConfig] instance of the default [FirebaseApp]. */
 expect val Firebase.config: FirebaseRemoteConfig
@@ -15,7 +16,7 @@ expect class FirebaseRemoteConfig {
     val configSettings: FirebaseRemoteConfigSettings
 
     suspend fun fetch()
-    suspend fun fetch(expiration: Double)
+    suspend fun fetch(expiration: Long)
     suspend fun fetchAndActivate(): Boolean
     suspend fun activate(): Boolean
     suspend fun ensureInitialized(): FirebaseRemoteConfigInfo
@@ -31,7 +32,16 @@ data class FirebaseRemoteConfigSettings(val fetchTimeout: Long? /* seconds */, v
 enum class FirebaseRemoteConfigSource(val value: Int) {
     DEFAULT(1),
     REMOTE(2),
-    STATIC(0)
+    STATIC(0);
+
+    companion object {
+        fun fromString(value: String): FirebaseRemoteConfigSource = when(value) {
+            "static" -> STATIC
+            "default" -> DEFAULT
+            "remote" -> REMOTE
+            else -> throw UnsupportedOperationException(value)
+        }
+    }
 }
 
 data class FirebaseRemoteConfigValue (
@@ -70,5 +80,20 @@ enum class FirebaseRemoteConfigFetchStatus(val value: Byte) {
     NO_FETCH_YET(0),
     SUCCESS(-1),
     FAILURE(1),
-    THROTTLED(2)
+    THROTTLED(2);
+
+    companion object {
+        fun fromString(value: String) = when(value) {
+            "no-fetch-yet" -> NO_FETCH_YET
+            "success" -> SUCCESS
+            "failure" -> FAILURE
+            "throttle" -> THROTTLED
+            else -> throw UnsupportedOperationException(value)
+        }
+    }
 }
+
+expect open class FirebaseRemoteConfigException : FirebaseException
+expect class FirebaseRemoteConfigClientException : FirebaseRemoteConfigException
+expect class FirebaseRemoteConfigFetchThrottledException : FirebaseRemoteConfigException
+expect class FirebaseRemoteConfigServerException : FirebaseRemoteConfigException
