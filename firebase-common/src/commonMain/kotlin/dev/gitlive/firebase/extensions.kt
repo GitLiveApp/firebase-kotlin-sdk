07197f61c877883c9dd5f432bac2e7d0486002cb
nbransby <nbransby@gmail.com>
2021-03-05 15:35:10 +0000

    avoid ChildCancelledException https://github.com/Kotlin/kotlinx.coroutines/issues/2550
    (cherry picked from commit 505ba3c986013ab240e046f067233137afccfd9d)

--- firebase-common/src/commonMain/kotlin/dev/gitlive/firebase/extensions.kt
+++ firebase-common/src/commonMain/kotlin/dev/gitlive/firebase/extensions.kt
@@ -6,8 +6,6 @@ import kotlinx.coroutines.channels.SendChannel
 
 //workaround for https://github.com/Kotlin/kotlinx.coroutines/issues/974
 @ExperimentalCoroutinesApi
-fun <E> SendChannel<E>.safeOffer(element: E) = try {
-    !isClosedForSend && offer(element)
-} catch (e : ClosedSendChannelException) {
-    false
+fun <E> SendChannel<E>.safeOffer(element: E): Boolean {
+    return runCatching { !isClosedForSend && offer(element) }.getOrDefault(false)
 }
