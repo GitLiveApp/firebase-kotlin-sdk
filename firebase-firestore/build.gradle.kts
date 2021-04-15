8d8de7a36d2cf69bebaae71c819d00a55bbcb53b
Sebastian Owodzin <sowodzin@redventures.com>
2021-04-15 13:59:30 +0100

    updated Kotlin, Android SDK & Gradle plugin

--- firebase-firestore/build.gradle.kts
+++ firebase-firestore/build.gradle.kts
@@ -9,7 +9,7 @@ version = project.property("firebase-firestore.version") as String
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
-    kotlin("plugin.serialization") version "1.4.31"
+    kotlin("plugin.serialization") version "1.4.32"
 }
 
 android {
