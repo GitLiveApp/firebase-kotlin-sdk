b66b77f746866d662a7518896bbe61c2fa693667
pauldavies83 <pauldavies83@googlemail.com>
2021-09-17 14:02:02 +0100

    ➕ Add iosSimulatorArm64 target, introduced in KMM 1.5.30 ⬆️ Bump kotlinx-coroutines to 1.5.2 (which adds the same targets) ⬆️ Bump kotlinx-serialization to 1.3.0-RC (which adds the same targets)

--- build.gradle.kts
+++ build.gradle.kts
@@ -207,12 +207,12 @@ subprojects {
         }
 
         dependencies {
-            "commonMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.1")
+            "commonMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+            "androidMainImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.2")
             "androidMainImplementation"(platform("com.google.firebase:firebase-bom:28.4.0"))
             "commonTestImplementation"(kotlin("test-common"))
             "commonTestImplementation"(kotlin("test-annotations-common"))
-            "commonTestImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+            "commonTestImplementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
             "jsTestImplementation"(kotlin("test-js"))
             "androidAndroidTestImplementation"(kotlin("test-junit"))
             "androidAndroidTestImplementation"("junit:junit:4.13.2")
