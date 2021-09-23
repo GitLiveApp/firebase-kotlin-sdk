96f12a61bf62f4152c2fbba6a28e55393f113bad
Andrew Reed <andrew_reed@hotmail.com>
2021-09-23 15:45:58 +0100

    Fixed tests

--- firebase-common/src/androidTest/kotlin/dev/gitlive/firebase/EncodersTest.kt
+++ firebase-common/src/androidTest/kotlin/dev/gitlive/firebase/EncodersTest.kt
@@ -4,44 +4,6 @@
 
 package dev.gitlive.firebase
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.serializer
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNull
-
-@Serializable
-data class TestData(val map: Map<String, String>, val bool: Boolean = false, val nullableBool: Boolean? = null)
-
-class EncodersTest {
-    @Test
-    fun encodeMap() {
-        val encoded = encode(mapOf("key" to "value"), shouldEncodeElementDefault = true)
-        assertEquals(mapOf("key" to "value"), encoded)
-    }
-
-    @Test
-    fun encodeObject() {
-        val encoded = encode<TestData>(TestData::class.serializer(), TestData(mapOf("key" to "value"), true), shouldEncodeElementDefault = false)
-        assertEquals(mapOf("map" to mapOf("key" to "value"), "bool" to true), encoded)
-    }
-
-    @Test
-    fun decodeObject() {
-        val decoded = decode<TestData>(TestData::class.serializer(), mapOf("map" to mapOf("key" to "value")))
-        assertEquals(TestData(mapOf("key" to "value"), false), decoded)
-    }
-
-    @Test
-    fun decodeListOfObjects() {
-        val decoded = decode(ListSerializer(TestData::class.serializer()), listOf(mapOf("map" to mapOf("key" to "value"))))
-        assertEquals(listOf(TestData(mapOf("key" to "value"), false)), decoded)
-    }
-
-    @Test
-    fun decodeObjectNullableValue() {
-        val decoded = decode<TestData>(TestData::class.serializer(), mapOf("map" to mapOf("key" to "value"), "nullableBool" to null))
-        assertNull(decoded.nullableBool)
-    }
-}
+actual fun nativeMapOf(vararg pairs: Pair<String, Any?>): Any = mapOf(*pairs)
+actual fun nativeListOf(vararg elements: Any): Any = listOf(*elements)
+actual fun nativeAssertEquals(expected: Any?, actual: Any?) = kotlin.test.assertEquals(expected, actual)
