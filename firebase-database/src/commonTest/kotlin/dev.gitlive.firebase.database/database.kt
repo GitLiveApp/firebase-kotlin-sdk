b559b999b9a787840074f357a184e7da8a76244d
Joey Heck <jheck@insulet.com>
2021-10-18 11:26:43 -0700

    setup unit testing for javascript in database

---
+++ firebase-database/src/commonTest/kotlin/dev.gitlive.firebase.database/database.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 GitLive Ltd.  Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.gitlive.firebase.database
+
+import dev.gitlive.firebase.*
+import kotlinx.coroutines.flow.first
+import kotlinx.serialization.*
+import kotlin.test.*
+
+expect val emulatorHost: String
+expect val context: Any
+expect fun runTest(test: suspend () -> Unit)
+
+class database {
+
+    @BeforeTest
+    fun initializeFirebase() {
+        Firebase
+            .takeIf { Firebase.apps(context).isEmpty() }
+            ?.apply {
+                initialize(
+                    context,
+                    FirebaseOptions(
+                        applicationId = "1:846484016111:ios:dd1f6688bad7af768c841a",
+                        apiKey = "AIzaSyCK87dcMFhzCz_kJVs2cT2AVlqOTLuyWV0",
+                        databaseUrl = "https://fir-kotlin-sdk.firebaseio.com",
+                        storageBucket = "fir-kotlin-sdk.appspot.com",
+                        projectId = "fir-kotlin-sdk",
+                        gcmSenderId = "846484016111"
+                    )
+                )
+                Firebase.database.useEmulator(emulatorHost, 8080)
+            }
+    }
+}
