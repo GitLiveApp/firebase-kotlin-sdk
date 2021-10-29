adbd47e31c992a161212b577f4752241a6fb52a0
Carmelo Gallo <work@carmelogallo.com>
2021-10-29 11:37:52 +0200

    Bumped serialization version to 1.3.0

--- firebase-common/build.gradle.kts
+++ firebase-common/build.gradle.kts
@@ -77,7 +77,7 @@ kotlin {
 
         val commonMain by getting {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.1.0")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.0")
             }
         }
 
