c5e313c7d7503549caae7355b55392bf1b89b5e5
corrado4eyes <corradoquattrocchi@hotmail.it>
2022-01-13 13:19:50 +0100

    Add helper methods.

--- firebase-firestore/src/commonMain/kotlin/dev/gitlive/firebase/firestore/encoders.kt
+++ firebase-firestore/src/commonMain/kotlin/dev/gitlive/firebase/firestore/encoders.kt
@@ -20,3 +20,17 @@ internal fun <T> encode(strategy: SerializationStrategy<T>, value: T, shouldEnco
         shouldEncodeElementDefault,
         FieldValue.serverTimestamp()
     )
+
+@PublishedApi
+internal fun <T> encodeAsMap(
+    strategy: SerializationStrategy<T>,
+    data: T,
+    encodeDefaults: Boolean = false
+): Map<String, Any?> = encode(strategy, data, encodeDefaults) as Map<String, Any?>
+
+@PublishedApi
+internal fun encodeAsMap(
+    encodeDefaults: Boolean = false,
+    vararg fieldsAndValues: Pair<String, Any?>
+): Map<String, Any?>? = fieldsAndValues.takeUnless { fieldsAndValues.isEmpty() }
+    ?.map { (field, value) -> field to encode(value, encodeDefaults) }?.toMap()
