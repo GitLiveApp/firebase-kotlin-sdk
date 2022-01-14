d3e8e8c740e09c55b37349ea25fbe260766bad9b
Carmelo Gallo <work@carmelogallo.com>
2022-01-14 15:43:15 +0100

    Bumped Kotlin version to 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,9 @@ import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-// TODO: To be removed once we will migrate to kotlin version 1.6.20
-// https://youtrack.jetbrains.com/issue/KT-49109#focus=Comments-27-5667134.0-0
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
-}
-
 plugins {
-    kotlin("multiplatform") apply false
-    kotlin("plugin.serialization") apply false
+    kotlin("multiplatform") version "1.6.10" apply false
+    kotlin("plugin.serialization") version "1.6.10" apply false
     id("base")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,3 @@ include(
     "firebase-firestore",
     "firebase-functions"
 )
-
-pluginManagement {
-    val kotlinVersion: String by settings
-
-    plugins {
-        kotlin("multiplatform") version kotlinVersion
-        kotlin("plugin.serialization") version kotlinVersion
-    }
-}
