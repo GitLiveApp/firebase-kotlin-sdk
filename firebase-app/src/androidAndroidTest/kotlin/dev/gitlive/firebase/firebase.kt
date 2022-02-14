3a5be0e52f38b96938c73c6c1b7b621743ec69c8
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2022-02-14 16:43:49 +0100

    test2

--- firebase-app/src/androidAndroidTest/kotlin/dev/gitlive/firebase/firebase.kt
+++ firebase-app/src/androidAndroidTest/kotlin/dev/gitlive/firebase/firebase.kt
@@ -11,3 +11,9 @@ import kotlinx.coroutines.runBlocking
 actual val context: Any = InstrumentationRegistry.getInstrumentation().targetContext
 
 actual fun runTest(test: suspend () -> Unit) = runBlocking { test() }
+
+object Firebase
+
+expect class FirebaseApp {
+    val name: String
+    val options: FirebaseOptions
