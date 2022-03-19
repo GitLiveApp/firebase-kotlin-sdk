6a08e16c23083b815e075410bb4d1a599da1be0d
Ignacio Ruiz <ignacio.ruizmartin@heetch.com>
2022-03-19 16:12:19 +0100

    Done on Android

--- firebase-common/src/androidMain/kotlin/dev/gitlive/firebase/_decoders.kt
+++ firebase-common/src/androidMain/kotlin/dev/gitlive/firebase/_decoders.kt
@@ -7,17 +7,20 @@ package dev.gitlive.firebase
 import kotlinx.serialization.encoding.CompositeDecoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.PolymorphicKind
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.StructureKind
 
-actual fun FirebaseDecoder.structureDecoder(descriptor: SerialDescriptor, decodeDouble: (value: Any?) -> Double?): CompositeDecoder = when(descriptor.kind as StructureKind) {
-        StructureKind.CLASS, StructureKind.OBJECT -> (value as Map<*, *>).let { map ->
+actual fun FirebaseDecoder.structureDecoder(descriptor: SerialDescriptor, decodeDouble: (value: Any?) -> Double?): CompositeDecoder =
+    when(descriptor.kind) {
+        StructureKind.CLASS, StructureKind.OBJECT, PolymorphicKind.SEALED -> (value as Map<*, *>).let { map ->
             FirebaseClassDecoder(decodeDouble, map.size, { map.containsKey(it) }) { desc, index -> map[desc.getElementName(index)] }
         }
-        StructureKind.LIST -> (value as List<*>).let {
+        StructureKind.LIST, PolymorphicKind.SEALED-> (value as List<*>).let {
             FirebaseCompositeDecoder(decodeDouble, it.size) { _, index -> it[index] }
         }
         StructureKind.MAP -> (value as Map<*, *>).entries.toList().let {
             FirebaseCompositeDecoder(decodeDouble, it.size) { _, index -> it[index/2].run { if(index % 2 == 0) key else value }  }
         }
+        else -> TODO("The firebase-kotlin-sdk does not support $descriptor for serialization yet")
     }
