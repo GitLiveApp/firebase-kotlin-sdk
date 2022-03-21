2bb2f07057d550a44333fe74fe887b7d721db1ff
Ignacio Ruiz <ignacio.ruizmartin@heetch.com>
2022-03-21 13:27:32 +0100

    Add testing

--- firebase-common/src/iosMain/kotlin/dev/gitlive/firebase/_decoders.kt
+++ firebase-common/src/iosMain/kotlin/dev/gitlive/firebase/_decoders.kt
@@ -5,8 +5,6 @@
 package dev.gitlive.firebase
 
 import kotlinx.serialization.encoding.CompositeDecoder
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.PolymorphicKind
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.StructureKind
