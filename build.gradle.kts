9ca07807c4e440bb7d7b6dd4ad072d38e4579d79
Michael Richardson <michael@richardsons.co.za>
2022-03-23 09:22:40 +0000

    use previous version of java-sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,7 +208,7 @@ subprojects {
         }
 
         dependencies {
-            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.7")
+            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.6")
             "jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.0") {
                 exclude("com.google.android.gms")
             }
