24069443b09983c5cb883e8f3118ffbc006608d9
Michael Richardson <michael@richardsons.co.za>
2022-04-08 09:54:02 +0100

    add tests startAfter and startAt

--- firebase-firestore/src/commonTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
+++ firebase-firestore/src/commonTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
@@ -234,10 +234,73 @@ class FirebaseFirestoreTest {
 
         val lastDocumentSnapshot = firstPage.lastOrNull()
         assertNotNull(lastDocumentSnapshot)
-        val secondPage = query.startAfter(lastDocumentSnapshot).limit(2).get().documents // Second 2 results (only one left)
+        
+        val secondPage = query.startAfter(lastDocumentSnapshot).limit(2).get().documents
         assertEquals(1, secondPage.size)
         assertEquals("ccc", secondPage[0].get("prop1"))
     }
+
+    @Test
+    fun testStartAfterFieldValues() = runTest {
+        setupFirestoreData()
+        val query = Firebase.firestore
+            .collection("testFirestoreQuerying")
+            .orderBy("prop1", Direction.ASCENDING)
+
+        val firstPage = query.limit(2).get().documents // First 2 results
+        assertEquals(2, firstPage.size)
+        assertEquals("aaa", firstPage[0].get("prop1"))
+        assertEquals("bbb", firstPage[1].get("prop1"))
+
+        val lastDocumentSnapshot = firstPage.lastOrNull()
+        assertNotNull(lastDocumentSnapshot)
+
+        val secondPage = query.startAfter("bbb").limit(2).get().documents
+        assertEquals(1, secondPage.size)
+        assertEquals("ccc", secondPage[0].get("prop1"))
+    }
+
+//    @Test
+//    fun testStartAtDocumentSnapshot() = runTest {
+//        setupFirestoreData()
+//        val query = Firebase.firestore
+//            .collection("testFirestoreQuerying")
+//            .orderBy("prop1", Direction.ASCENDING)
+//
+//        val firstPage = query.limit(2).get().documents // First 2 results
+//        assertEquals(2, firstPage.size)
+//        
+//        assertEquals("aaa", firstPage[0].get("prop1"))
+//        assertEquals("bbb", firstPage[1].get("prop1"))
+//
+//        val lastDocumentSnapshot = firstPage.lastOrNull()
+//        assertNotNull(lastDocumentSnapshot)
+//        
+//        val secondPage = query.startAt(lastDocumentSnapshot).limit(2).get().documents
+//        assertEquals(2, secondPage.size)
+//        assertEquals("bbb", secondPage[0].get("prop1"))
+//        assertEquals("ccc", secondPage[1].get("prop1"))
+//    }
+//
+//    @Test
+//    fun testStartAtFieldValues() = runTest {
+//        setupFirestoreData()
+//        val query = Firebase.firestore
+//            .collection("testFirestoreQuerying")
+//            .orderBy("prop1", Direction.ASCENDING)
+//
+//        val firstPage = query.limit(2).get().documents // First 2 results
+//        assertEquals(2, firstPage.size)
+//        assertEquals("aaa", firstPage[0].get("prop1"))
+//        assertEquals("bbb", firstPage[1].get("prop1"))
+//
+//        val lastDocumentSnapshot = firstPage.lastOrNull()
+//        assertNotNull(lastDocumentSnapshot)
+//        val secondPage = query.startAfter("bbb").limit(2).get().documents
+//        assertEquals(2, secondPage.size)
+//        assertEquals("bbb", secondPage[0].get("prop1"))
+//        assertEquals("ccc", secondPage[1].get("prop1"))
+//    }
 
     @Test
     fun testIncrementFieldValue() = runTest {
