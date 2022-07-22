199051180c7374335a4005786460c3e55db7a460
Michael Richardson <michael@richardsons.co.za>
2022-07-22 15:51:47 +0100

    update java sdk to 1.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,7 +208,7 @@ subprojects {
         }
 
         dependencies {
-            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.12-1.6.21-d3cbe60")
+            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.12")
             "jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.0") {
                 exclude("com.google.android.gms")
             }
