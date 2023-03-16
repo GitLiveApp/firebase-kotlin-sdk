18e04d5d18189522d8782a941ffc32a848381e44
Oliver McBurney <omcburney25@gmail.com>
2023-03-16 16:39:08 +0000

    [trace-expansion] Remove actual from PerfSession

--- firebase-perf/src/androidMain/kotlin/dev/gitlive/firebase/perf/session/PerfSession.kt
+++ firebase-perf/src/androidMain/kotlin/dev/gitlive/firebase/perf/session/PerfSession.kt
@@ -2,5 +2,5 @@ package dev.gitlive.firebase.perf.session
 
 import com.google.firebase.perf.session.PerfSession
 
-actual class PerfSession internal constructor(val android: PerfSession) {
+class PerfSession internal constructor(val android: PerfSession) {
 }
