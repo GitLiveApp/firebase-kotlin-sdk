2843d4cb88b69110f1a4e80703c970b4eca46224
Andrew Reed <andrew_reed@hotmail.com>
2023-03-29 17:22:04 +0100

    made android run blocking for firestore tests due to failure

--- firebase-firestore/src/androidAndroidTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
+++ firebase-firestore/src/androidAndroidTest/kotlin/dev/gitlive/firebase/firestore/firestore.kt
@@ -7,9 +7,10 @@ package dev.gitlive.firebase.firestore
 
 import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
 
 actual val emulatorHost: String = "10.0.2.2"
 
 actual val context: Any = InstrumentationRegistry.getInstrumentation().targetContext
 
-actual fun runTest(test: suspend CoroutineScope.() -> Unit) = kotlinx.coroutines.test.runTest { test() }
+actual fun runTest(test: suspend CoroutineScope.() -> Unit) = runBlocking { test() }
