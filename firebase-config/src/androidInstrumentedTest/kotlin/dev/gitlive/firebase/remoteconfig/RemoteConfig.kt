956977510eec01942a529a05b80bf0ac1406196e
Andrew Reed <andrew_reed@hotmail.com>
2023-06-13 18:03:46 +0100

    Changes to new android source layout

---
+++ firebase-config/src/androidInstrumentedTest/kotlin/dev/gitlive/firebase/remoteconfig/RemoteConfig.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 GitLive Ltd.  Use of this source code is governed by the Apache 2.0 license.
+ */
+
+@file:JvmName("tests")
+package dev.gitlive.firebase.remoteconfig
+
+import androidx.test.platform.app.InstrumentationRegistry
+
+actual val context: Any = InstrumentationRegistry.getInstrumentation().targetContext
+
+actual fun runTest(test: suspend () -> Unit) = kotlinx.coroutines.test.runTest { test() }
