6755ff9df10846b3aadbc4b8a9d16e92cb736a39
Gijs van Veen <gijs@splendo.com>
2023-09-26 15:11:43 +0200

    More timeouts

--- firebase-database/src/jsTest/kotlin/dev/gitlive/firebase/database/database.kt
+++ firebase-database/src/jsTest/kotlin/dev/gitlive/firebase/database/database.kt
@@ -8,12 +8,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 actual val emulatorHost: String = "127.0.0.1" // in JS tests connection is refused if we use localhost
 
 actual val context: Any = Unit
-actual fun runTest(test: suspend () -> Unit) = kotlinx.coroutines.test.runTest {
+actual fun runTest(test: suspend () -> Unit) = kotlinx.coroutines.test.runTest(timeout = 5.minutes) {
     // in JS tests we need to wait for the database to be connected
     awaitDatabaseConnection()
     test()
