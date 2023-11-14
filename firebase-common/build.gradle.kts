326a7cd9f6a21118f988ddcaa4aa9815b4a24906
Gijs van Veen <gijs@splendo.com>
2023-11-14 20:16:44 +0100

    Remove instrumentation tests from FB Common

--- firebase-common/build.gradle.kts
+++ firebase-common/build.gradle.kts
@@ -54,8 +54,6 @@ kotlin {
 
     @Suppress("OPT_IN_USAGE")
     androidTarget {
-        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-        unitTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
         publishAllLibraryVariants()
         compilations.configureEach {
             kotlinOptions {
