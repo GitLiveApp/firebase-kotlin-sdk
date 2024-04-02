aabf55efb0302e12153c7be7aeaadb25cad17ee0
Andrew Reed <andrew_reed@hotmail.com>
2024-04-02 22:05:31 +0100

    cocoapods version needs to be fuzzy

--- firebase-functions/build.gradle.kts
+++ firebase-functions/build.gradle.kts
@@ -77,7 +77,7 @@ kotlin {
             }
             noPodspec()
             pod("FirebaseFunctions") {
-                version = "10.19.0"
+                version = "~> 10.19"
                 extraOpts += listOf("-compiler-option", "-fmodules")
             }
         }
