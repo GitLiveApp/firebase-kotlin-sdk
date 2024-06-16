ddb283fec1557c1760d82d22ba013288d4459ad2
Bas Buijsen <b.buijsen@egeniq.com>
2024-06-16 17:20:24 +0200

    fix js implementation

--- firebase-messaging/src/jsMain/kotlin/dev/gitlive/firebase/messaging/messaging.kt
+++ firebase-messaging/src/jsMain/kotlin/dev/gitlive/firebase/messaging/messaging.kt
@@ -3,24 +3,21 @@ package dev.gitlive.firebase.messaging
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.messaging.externals.Messaging
 import dev.gitlive.firebase.messaging.externals.getMessaging
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.await
-import kotlinx.coroutines.launch
+
 
 actual val Firebase.messaging: FirebaseMessaging
     get() = FirebaseMessaging(getMessaging())
 
 actual class FirebaseMessaging(val js: Messaging) {
     actual fun subscribeToTopic(topic: String) {
-        GlobalScope.launch {
-            js.subscribeToTopic(arrayOf(getToken()), topic)
-        }
+        // This is not supported in the JS SDK
+        // https://firebase.google.com/docs/reference/js/messaging_.md#@firebase/messaging
     }
 
     actual fun unsubscribeFromTopic(topic: String) {
-        GlobalScope.launch {
-            js.unsubscribeFromTopic(arrayOf(getToken()), topic)
-        }
+        // This is not supported in the JS SDK
+        // https://firebase.google.com/docs/reference/js/messaging_.md#@firebase/messaging
     }
 
     actual suspend fun getToken(): String = dev.gitlive.firebase.messaging.externals.getToken(js).await()
