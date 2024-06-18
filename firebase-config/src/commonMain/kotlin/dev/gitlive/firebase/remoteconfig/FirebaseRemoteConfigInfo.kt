d7962a83c21e7570b37a3eebd86e4df028bd159c
Bas Buijsen <b.buijsen@egeniq.com>
2024-06-18 15:26:24 +0200

    add documentation for remote config, crashlytics and database

--- firebase-config/src/commonMain/kotlin/dev/gitlive/firebase/remoteconfig/FirebaseRemoteConfigInfo.kt
+++ firebase-config/src/commonMain/kotlin/dev/gitlive/firebase/remoteconfig/FirebaseRemoteConfigInfo.kt
@@ -1,9 +1,53 @@
 package dev.gitlive.firebase.remoteconfig
 
+/** Wraps the current state of the [FirebaseRemoteConfig] singleton object. */
 data class FirebaseRemoteConfigInfo(
+    /**
+     * Gets the current settings of the [FirebaseRemoteConfig] singleton object.
+     *
+     * @return A [FirebaseRemoteConfig] object indicating the current settings.
+     */
     val configSettings: FirebaseRemoteConfigSettings,
+
+    /**
+     * Gets the timestamp (milliseconds since epoch) of the last successful fetch, regardless of
+     * whether the fetch was activated or not.
+     *
+     * @return -1 if no fetch attempt has been made yet. Otherwise, returns the timestamp of the last
+     *     successful fetch operation.
+     */
     val fetchTimeMillis: Long,
+
+    /**
+     * Gets the status of the most recent fetch attempt.
+     *
+     * @return Will return one of [FetchStatus.Success], [FetchStatus.Failure], [FetchStatus.Throttled], or [FetchStatus.NoFetchYet]
+     */
     val lastFetchStatus: FetchStatus,
 )
 
-enum class FetchStatus { Success, Failure, Throttled, NoFetchYet }
+enum class FetchStatus {
+    /**
+     * Indicates that the most recent fetch of parameter values from the Firebase Remote Config server
+     * was completed successfully.
+     */
+    Success,
+
+    /**
+     * Indicates that the most recent attempt to fetch parameter values from the Firebase Remote
+     * Config server has failed.
+     */
+    Failure,
+
+    /**
+     * Indicates that the most recent attempt to fetch parameter values from the Firebase Remote
+     * Config server was throttled.
+     */
+    Throttled,
+
+    /**
+     * Indicates that the FirebaseRemoteConfig singleton object has not yet attempted to fetch
+     * parameter values from the Firebase Remote Config server.
+     */
+    NoFetchYet
+}
