32dcb6b7b90b08be4da5237523381c4808845ac3
Michael Richardson <michael@richardsons.co.za>
2026-05-28 06:54:44 +0100

    add purgeOutstandingWrites to api

--- firebase-database/src/jsMain/kotlin/dev/gitlive/firebase/database/database.kt
+++ firebase-database/src/jsMain/kotlin/dev/gitlive/firebase/database/database.kt
@@ -85,6 +85,8 @@ public actual class FirebaseDatabase internal constructor(internal val js: Datab
     public actual fun goOffline(): Unit = rethrow { jsGoOffline(js) }
 
     public actual fun goOnline(): Unit = rethrow { jsGoOnline(js) }
+
+    public actual fun purgeOutstandingWrites(): Unit = throw UnsupportedOperationException("purgeOutstandingWrites is not supported by the Firebase Realtime Database JS SDK.")
 }
 
 internal actual open class NativeQuery(
